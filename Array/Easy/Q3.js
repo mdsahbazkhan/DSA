33492d3103e3e130d0c46909595754687f0f0b31
Sahbaz khan <amdsahbaz19@gmail.com>
2025-08-13 17:29:18 +0530

    Check if Array Is Sorted and Rotated

--- Array/Easy/Q3.js
+++ Array/Easy/Q3.js
@@ -1,1 +1,15 @@
 // Check if Array Is Sorted and Rotated
+function isSortedAndRotated(arr) {
+let n=arr.length;
+let count = 0;
+for(let i=1;i<n;i++){
+    if(arr[i]<arr[i-1]){
+        count++
+    }
+}
+if(arr[n-1]>arr[0]){
+    count++
+}
+return count<=1;
+}
+console.log(isSortedAndRotated([1,2,3]));
