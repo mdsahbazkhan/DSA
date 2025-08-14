07c95bc6877c5b887337bae25ec770431d1d49be
Sahbaz khan <amdsahbaz19@gmail.com>
2025-08-14 13:16:23 +0530

    find char freq

--- Array/Easy/Q3.js
+++ Array/Easy/Q3.js
@@ -1,15 +1,29 @@
 // Check if Array Is Sorted and Rotated
-function isSortedAndRotated(arr) {
-let n=arr.length;
-let count = 0;
-for(let i=1;i<n;i++){
-    if(arr[i]<arr[i-1]){
-        count++
+// function isSortedAndRotated(arr) {
+// let n=arr.length;
+// let count = 0;
+// for(let i=1;i<n;i++){
+//     if(arr[i]<arr[i-1]){
+//         count++
+//     }
+// }
+// if(arr[n-1]>arr[0]){
+//     count++
+// }
+// return count<=1;
+// }
+// console.log(isSortedAndRotated([1,2,3]));
+
+
+
+function charFrequency(str){
+    let frqstr ={}
+    for (let num of str) {
+        frqstr[num]=(frqstr[num]||0)+1
+        
     }
+    return frqstr
+
 }
-if(arr[n-1]>arr[0]){
-    count++
-}
-return count<=1;
-}
-console.log(isSortedAndRotated([1,2,3]));
+console.log(charFrequency("shahnaz"));
+
