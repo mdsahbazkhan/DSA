5c21f5c32a23aca1fa5917761cae82e5260df131
Sahbaz khan <amdsahbaz19@gmail.com>
2025-08-18 17:22:29 +0530

    divide easy ,medium and hard problem

--- Array/Easy/Q6.js
+++ Array/Easy/Q6.js
@@ -12,48 +12,4 @@
 // console.log(rotateArr([1,2,3,4,5]));
 
 
-//Right rotate by k places
-function RightRotate(arr,k) {
-let n = arr.length;
-k=k%n;
-let temp=arr.slice(n-k,n)
-console.log(temp);
 
-for(let i=n-k-1;i>=0;i--){
-    arr[i+k]=arr[i];
-}
-for (let i = 0; i < k; i++) {
-   arr[i] = temp[i];
-    
-}
-return arr;
-    
-}
-console.log(RightRotate([1,2,3,4,5,6], 2));
-
-
-
-
-
-
-// function RightRotate(arr,k) {
-//     let n = arr.length;
-//     k = k % n;
-//     reverse(arr, 0, n - 1);
-//     reverse(arr,0,k-1);
-//     reverse(arr,k,n-1);
-//     return arr;
-    
-// }
-// function reverse(arr,low,high){
-//     while(low<high){
-//         let temp = arr[low];
-//         arr[low] = arr[high];
-//         arr[high] = temp;
-//         low++;
-//         high--;
-//     }
-
-// }
-// console.log(RightRotate([1,2,3,4,5,6,7
-//     ],3));
---
+++ Array/Medium/Q2.js
@@ -0,0 +1,39 @@
+// Left rotate by k places
+
+// Brute force
+// function leftRotate(arr, k) {
+//     let n = arr.length;
+//     k=k%n;
+//     let temp=arr.slice(0,k);
+    
+//     for(let i=k;i<n;i++){
+//         arr[i-k]=arr[i];
+//     }
+//     for(let i=n-k;i<n;i++){
+//         arr[i]=temp[i-(n-k)]
+//     }
+//     return arr;
+// }
+// console.log(leftRotate([1,2,3,4,5,6,7,8],18));
+
+
+//Optimal solution
+// function leftRotate(arr, k) {
+//     let n = arr.length;
+//     k=k%n;
+//     reverse(arr,0,k-1);
+//     reverse(arr,k,n-1);
+//     reverse(arr,0,n-1);
+//     return arr;
+// }
+// function reverse(arr, start, end) {
+//     while(start<end){
+//         let temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
+// console.log(leftRotate([1,2,3,4,5,6,7,8
+//     ],3));
