f22a361cb83d4b7f8c578bd7095c49c2bebb4fc8
Sahbaz khan <amdsahbaz19@gmail.com>
2025-08-22 19:01:22 +0530

    Longest subArray with sum K (+ve and -ve) element

--- Array/Easy/Q14.js
+++ Array/Easy/Q14.js
@@ -2,44 +2,66 @@
 
 //Brute force 
 // function subArray(arr,k){
-//     let n= arr.length
-//     let maxlength=0;
-//     for(i=0;i<n;i++){
+//     let n= arr.length;
+//     let maxLength=0;
+//     for(let i=0;i<n;i++){
 //         let sum=0;
-//         for(let j=i;j<n;j++){              
-//                 sum+=arr[j];
+//         for(let j=i;j<n;j++){
+//             sum+=arr[j];
 //             if(sum==k){
-//                 maxlength=Math.max(maxlength,j-i+1)
+//                 maxLength=Math.max(maxLength,j-i+1);
 //             }
 //         }
 //     }
-//     return maxlength
-// }
-// console.log(subArray([1,2,3,1,1], 6));
+//     return maxLength;
+// };
+// console.log(subArray([1,2,3,1,1,1,1,4,2,3],3));
 
-function subArray(arr,k){
-    let map=new Map()
-    let n= arr.length;
-    let sum=0;
-    let maxlength=0;
-    for(let i=0;i<n;i++){
-        sum+=arr[i];
-        if(sum==k){
-            maxlength=Math.max(maxlength,i+1);
-        }
-        if(map.has(sum-k)){
-            maxlength= Math.max(maxlength,i-map.get(sum-k))
-        }
-        if(map.has(sum)){
-            
-            map.set(sum,i)
-            
-        }
-        
-    }
-     
-    
-    return maxlength
-   
-}
-console.log(subArray([1,2,3,1,1,1,1,4,2,3], 6));
+//Better
+
+// function subArray(arr,k){
+//     let n= arr.length;
+//     let hash= new Map();
+//     let maxLength=0;
+//     let prefixSum=0;
+//     for(let i=0;i<n;i++){
+//         prefixSum+=arr[i];
+//         if(prefixSum==k){
+//             maxLength= Math.max(maxLength,i+1);
+//         }
+//         if(hash.has(prefixSum-k)){
+//             maxLength=Math.max(maxLength,i-(hash.get(prefixSum-k)));
+//         }
+//         if(!hash.has(prefixSum)){
+//             hash.set(prefixSum,i)
+//         }
+//     }
+//     return maxLength;
+// };
+// console.log(subArray([2,0,0,3],3));
+
+// Optimal
+//  function subArray(arr,k){
+//     let n= arr.length;
+//     let count=0;
+//     let sum=arr[0];
+//     let left=0;
+//     let right=0;
+//     while(right<n){
+//         while(left<=right && sum>k){
+//             sum-=arr[left];
+//             left++;
+//         }
+//         if(sum==k){
+//             maxLength=Math.max(maxLength,right-left+1);
+//         }
+//         right++;
+//         if(right<n){
+//             sum+=arr[right]
+//         }
+//     }
+
+
+//     return maxLength;
+// };
+// console.log(subArray([1,2,1,2,1],3));
