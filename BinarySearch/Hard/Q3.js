8acba2b0ed943f36fd8984b81c2853c3f9ea0cbd
sahbaz23 <amdsahbaz19@gmail.com>
2025-10-03 11:19:25 +0530

    c

--- BinarySearch/Hard/Q3.js
+++ BinarySearch/Hard/Q3.js
@@ -82,32 +82,33 @@
 function findMedianSortedArrays(arr1, arr2) {
   let n1 = arr1.length;
   let n2 = arr2.length;
-  if (n1 > n2) return findMedianSortedArrays(arr2, arr1);
   let n = n1 + n2;
-  let left = Math.floor((n + 1) / 2);
+  if (n1 > n2) return findMedianSortedArrays(arr2, arr1);
   let low = 0;
   let high = n1;
+  let left = Math.floor((n + 1) / 2);
   while (low <= high) {
     let mid1 = Math.floor((low + high) / 2);
     let mid2 = left - mid1;
     let l1 = Number.MIN_SAFE_INTEGER;
     let l2 = Number.MIN_SAFE_INTEGER;
-    let r2 = Number.MAX_SAFE_INTEGER;
     let r1 = Number.MAX_SAFE_INTEGER;
-
-    if (mid1 < n1) r1 = arr1[mid1]; // right element of a
-    if (mid2 < n2) r2 = arr2[mid2]; // right element of b
-    if (mid1 - 1 >= 0) l1 = arr1[mid1 - 1]; // left element of a
-    if (mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
+    let r2 = Number.MAX_SAFE_INTEGER;
+    if (mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
+    if (mid1 < n1) r1 = arr1[mid1];
+    if (mid2 - 1 >= 0) l1 = arr2[mid2 - 1];
+    if (mid2 < n1) r2 = arr2[mid2];
     if (l1 <= r2 && l2 <= r1) {
-      if (n % 2 === 1) return Math.max(l1, l2);
-      else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+      if (n % 2 == 0) {
+        return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+      } else {
+        return Math.max(l1, l2);
+      }
     } else if (l1 > r2) {
       high = mid1 - 1;
     } else {
       low = mid1 + 1;
     }
   }
-  return 0;
 }
-console.log(findMedianSortedArrays([1, 3, 4, 7, 10, 12], [2, 3, 6, 15]));
+console.log(findMedianSortedArrays([2, 3, 6, 15], [1, 3, 4, 7, 10, 12]));
