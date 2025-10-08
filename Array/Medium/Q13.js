5824595281d37575ef668d98c51fa3f18a789a56
sahbaz23 <amdsahbaz19@gmail.com>
2025-10-08 19:47:06 +0530

    ss

--- Array/Medium/Q13.js
+++ Array/Medium/Q13.js
@@ -1,6 +1,6 @@
 // 3Sum Problem
 
-// Brute 
+// Brute
 
 // function threeSum(arr) {
 //     let n= arr.length;
@@ -21,7 +21,7 @@
 //         ans.push(triplet.split(",").map(Number));
 //     }
 //     return ans
-       
+
 // }
 // const arr=[-1,0,1,2,-1,-4];
 // console.log(threeSum(arr));
@@ -48,7 +48,7 @@
 //         ans.push(triplet.split(",").map(Number));
 //     }
 //     return ans
-       
+
 // }
 // const arr=[-1,0,1,2,-1,-4];
 // console.log(threeSum(arr));
@@ -56,32 +56,30 @@
 //Optimal
 
 function threeSum(arr) {
-    let n= arr.length;
-    arr.sort((a,b)=>a-b);
-    let ans=[]
-    for(let i=0;i<n;i++){
-    let j=i+1;
-    let k=n-1;
-     if(arr[i]==arr[i-1])continue;
-    while(j<k){
-   let sum= arr[i]+arr[j]+arr[k];
-   if(sum<0){
-    j++;
-   }else if(sum>0){
-    k--;
-   }else{
-    let temp=[arr[i],arr[j],arr[k]];
-    ans.push(temp);
-    j++;
-    k--;
-    while(j<k&&arr[j]==arr[j-1])j++;
-    while(j<k&&arr[k]==arr[k+1])k--;
-
-   }
-}
+  let n = arr.length;
+  arr.sort((a, b) => a - b);
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    let j = i + 1;
+    let k = n - 1;
+    if (arr[i] == arr[i - 1]) continue;
+    while (j < k) {
+      let sum = arr[i] + arr[j] + arr[k];
+      if (sum < 0) {
+        j++;
+      } else if (sum > 0) {
+        k--;
+      } else {
+        let temp = [arr[i], arr[j], arr[k]];
+        ans.push(temp);
+        j++;
+        k--;
+        while (j < k && arr[j] == arr[j - 1]) j++;
+        while (j < k && arr[k] == arr[k + 1]) k--;
+      }
     }
-    return ans;
-       
+  }
+  return ans;
 }
-const arr=[-1,0,1,2,-1,-4];
+const arr = [-1, 0, 1, 2, -1, -4];
 console.log(threeSum(arr));
