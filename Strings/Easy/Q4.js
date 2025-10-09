e4f2cb98c8c1f9b0ebb0a26014cf57195610c617
sahbaz23 <amdsahbaz19@gmail.com>
2025-10-09 14:16:55 +0530

    Q4) Isomorphic Strings

---
+++ Strings/Easy/Q4.js
@@ -0,0 +1,20 @@
+//  Isomorphic Strings
+function isomorphic(s, t) {
+  if (s.length != t.length) return false;
+  let mapST = {};
+  let mapTS = {};
+  for (let i = 0; i < s.length; i++) {
+    let charS = s[i];
+    let charT = t[i];
+    if (
+      (mapST[charS] && mapST[charS] !== charT) ||
+      (mapTS[charT] && mapTS[charT] !== charS)
+    ) {
+      return false;
+    }
+    mapST[charS] = charT;
+    mapTS[charT] = charS;
+  }
+  return true;
+}
+console.log(isomorphic("paper", "title"));
