05ae893dcf89082b19a38ff37403e307100c69f2
sahbaz23 <amdsahbaz19@gmail.com>
2025-10-12 21:08:56 +0530

    fix

--- Array/Medium/Q9.js
+++ Array/Medium/Q9.js
@@ -1,7 +1,6 @@
 // Rotate matrix by 90 degree
 
-
-//Brute Force 
+//Brute Force
 // function rotateMatrix(matrix) {
 //     let n=matrix.length;
 //     let result=Array.from({length:n},()=>Array(n).fill(0));
@@ -11,34 +10,40 @@
 //         }
 //     }
 //     return result;
-    
+
 // }
 // console.log(rotateMatrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]));
 
-// function rotateMatrix(matrix) {
-//     let n=matrix.length;
-//      for(let i=0;i<n;i++){
-//         for(let j=i+1;j<n;j++){
-//             // [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]]
-//             let temp=matrix[i][j];
-//             matrix[i][j]=matrix[j][i];
-//             matrix[j][i]=temp
-//         }
-//      }
-//     //  for(let i=0;i<n;i++){
-//     //     matrix[i].reverse();
-//     //  }
+function rotateMatrix(matrix) {
+  let n = matrix.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      // [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]]
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  //  for(let i=0;i<n;i++){
+  //     matrix[i].reverse();
+  //  }
 
-//       for (let i = 0; i < n; i++) {
-//     let left = 0, right = n - 1;
-//     while (left < right) {
-//       [matrix[i][left], matrix[i][right]] = [matrix[i][right], matrix[i][left]];
-//       left++;
-//       right--;
-//     }
-//   }
-//      return matrix
-    
-// }
-// console.log(rotateMatrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]));
-
+  for (let i = 0; i < n; i++) {
+    let left = 0,
+      right = n - 1;
+    while (left < right) {
+      [matrix[i][left], matrix[i][right]] = [matrix[i][right], matrix[i][left]];
+      left++;
+      right--;
+    }
+  }
+  return matrix;
+}
+console.log(
+  rotateMatrix([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+  ])
+);
