af6078bfa21e71c9461bbca3e1b87e10d056e617
sahbaz23 <amdsahbaz19@gmail.com>
2025-12-16 18:55:09 +0530

    Palindrome Partitioning(Medium)

---
+++ Recursion/Medium/Q11.js
@@ -0,0 +1,27 @@
+var partition = function (s) {
+  let res = [];
+  function backtrack(idx, curr) {
+    if (idx === s.length) {
+      res.push([...curr]);
+      return;
+    }
+    for (let i = idx; i < s.length; i++) {
+      if (isPalindrome(s, idx, i)) {
+        curr.push(s.substring(idx, i + 1));
+        backtrack(i + 1, curr);
+        curr.pop();
+      }
+    }
+  }
+  backtrack(0, []);
+  return res;
+};
+function isPalindrome(s, left, right) {
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+console.log(partition("aab"));
