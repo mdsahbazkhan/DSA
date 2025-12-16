05cadadbcb6ddb44443b19d2c19f364fa6f751ff
sahbaz23 <amdsahbaz19@gmail.com>
2025-12-16 10:44:15 +0530

    fix

--- Sorting/SelectionSort.js
+++ Sorting/SelectionSort.js
@@ -1,16 +1,16 @@
-function insertionSort(arr){
-    let n=arr.length;
-    for(let i=0;i<n-1;i++){
-        let min=i;
-        for(let j=i;j<n;j++){
-            if(arr[j]<arr[min]){
-                min=j;
-            }
-        }
-        let temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
     }
-    return arr;
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+  return arr;
 }
-console.log(insertionSort([64,25,-12,22,11]));
+console.log(selectionSort([64, 25, -12, 22, 11]));
