9bdc534340d10e215a5714f861dadc8fee2c0703
sahbaz23 <amdsahbaz19@gmail.com>
2026-01-02 11:50:41 +0530

    Covertion Infix to prefix and Postfix

--- Stack&Queue/Easy/Q5.js
+++ Stack&Queue/Easy/Q5.js
@@ -1,8 +1,59 @@
 // Convert Infix to PostFix
 
-function infixToPostfix(s) {
+// function infixToPostfix(s) {
+//   let st = [];
+//   let ans = "";
+
+//   const prec = {
+//     "+": 1,
+//     "-": 1,
+//     "*": 2,
+//     "/": 2,
+//     "^": 3,
+//   };
+//   for (let ch of s) {
+//     if (/[a-zA-Z0-9]/.test(ch)) {
+//       ans += ch;
+//     } else if (ch === "(") st.push(ch);
+//     else if (ch === ")") {
+//       while (st.length && st[st.length - 1] !== "(") {
+//         ans += st.pop();
+//       }
+//       st.pop();
+//     } else {
+//       while (
+//         st.length &&
+//         st[st.length - 1] !== "(" &&
+//         (prec[st[st.length - 1]] > prec[ch] ||
+//           (prec[st[st.length - 1]] === prec[ch] && ch !== "^"))
+//       ) {
+//         ans += st.pop();
+//       }
+//       st.push(ch);
+//     }
+//   }
+//   while (st.length) {
+//     ans += st.pop();
+//   }
+//   return ans;
+// }
+// console.log(infixToPostfix("a^b^c"));
+
+//Convert Infix to Prefix
+function infixToPrefix(s) {
+  s = s.split("").reverse().join("");
+  let swapped = "";
+  for (let ch of s) {
+    if (ch === ")") {
+      swapped += "(";
+    } else if (ch === "(") {
+      swapped += ")";
+    } else {
+      swapped += ch;
+    }
+  }
   let st = [];
-  let ans = "";
+  let postfix = "";
 
   const prec = {
     "+": 1,
@@ -11,26 +62,30 @@ function infixToPostfix(s) {
     "/": 2,
     "^": 3,
   };
-  for (let ch of s) {
+  for (let ch of swapped) {
     if (/[a-zA-Z0-9]/.test(ch)) {
-      ans += ch;
-    } else if (ch == "(") st.push(ch);
-    else if (ch == ")") {
+      postfix += ch;
+    } else if (ch === "(") st.push(ch);
+    else if (ch === ")") {
       while (st.length && st[st.length - 1] !== "(") {
-        ans += st.pop();
+        postfix += st.pop();
       }
       st.pop();
     } else {
-      while (st.length && prec[st[st.length - 1]] >= prec[ch]) {
-        ans += st.pop();
+      while (
+        st.length &&
+        st[st.length - 1] !== "(" &&
+        (prec[st[st.length - 1]] > prec[ch] ||
+          (prec[st[st.length - 1]] === prec[ch] && ch === "^"))
+      ) {
+        postfix += st.pop();
       }
       st.push(ch);
     }
   }
   while (st.length) {
-    ans += st.pop();
+    postfix += st.pop();
   }
-  return ans;
+  return postfix.split("").reverse().join("");
 }
-
-console.log(infixToPostfix("a+b*(c^d-e)"));
+console.log(infixToPrefix("a^b^c"));
