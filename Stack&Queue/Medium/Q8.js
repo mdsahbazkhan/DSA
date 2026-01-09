509506cc292c0a85a1ad283f6bc3318e3aaf4509
sahbaz23 <amdsahbaz19@gmail.com>
2026-01-09 16:46:06 +0530

    The Celebrity Problem

---
+++ Stack&Queue/Medium/Q8.js
@@ -0,0 +1,24 @@
+//The Celebrity Problem
+
+function findCelebrity(n, knows) {
+    let st = [];
+    for (let i = 0; i < n; i++){
+        st.push(i);
+    }
+    while (st.length > 1) {
+        let i = st[st.length - 1]; st.pop();
+        let j = st[st.length - 1]; st.pop();
+        if (knows(i,j) === false) {
+            st.push(i);
+        } else {
+            st.push(j);
+        }
+    }
+    let celeb = st[st.length - 1];
+    for (let i = 0; i < n; i++){
+        if (i != celeb && (knows(i,celeb) == false || knows(celeb,i) == true)) {
+            return -1;
+        }
+    }
+    return celeb;
+}
