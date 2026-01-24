11cc73f777f4ddc4bdbff06a39a96bed36ac91c3
sahbaz23 <amdsahbaz19@gmail.com>
2026-01-24 12:06:17 +0530

    Binary Tree Inorder Traversal

---
+++ Binary Tree/Easy/Q2.js
@@ -0,0 +1,13 @@
+// Binary Tree Inorder Traversal
+
+var inorderTraversal = function (root) {
+    let result = [];
+    function dfs(root) {
+        if (root === null) return;
+        dfs(root.left);
+        result.push(root.val);
+        dfs(root.right);
+    }
+    dfs(root);
+    return result;
+};
