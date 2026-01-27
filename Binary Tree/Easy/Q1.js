17107b84dacc14e09f6b89da22de9338394c0bb5
sahbaz23 <amdsahbaz19@gmail.com>
2026-01-27 10:43:33 +0530

    iterative

--- Binary Tree/Easy/Q1.js
+++ Binary Tree/Easy/Q1.js
@@ -1,14 +1,28 @@
 //Binary Tree Preorder Traversal
 
-var preorderTraversal = function (root) {
-  let result = [];
-  function dfs(root) {
-    if (root === null) return;
+// var preorderTraversal = function (root) {
+//   let result = [];
+//   function dfs(root) {
+//     if (root === null) return;
 
-    result.push(root.val);
-    dfs(root.left);
-    dfs(root.right);
+//     result.push(root.val);
+//     dfs(root.left);
+//     dfs(root.right);
+//   }
+//   dfs(root);
+//   return result;
+// };
+
+function preorderTraversal(root) {
+  let result = [];
+  if (root === null) return result;
+  let stack = [];
+  stack.push(root);
+  while (stack.length !== 0) {
+    let node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
   }
-  dfs(root);
   return result;
-};
+}
--- Binary Tree/Easy/Q2.js
+++ Binary Tree/Easy/Q2.js
@@ -1,13 +1,29 @@
 // Binary Tree Inorder Traversal
 
-var inorderTraversal = function (root) {
-    let result = [];
-    function dfs(root) {
-        if (root === null) return;
-        dfs(root.left);
-        result.push(root.val);
-        dfs(root.right);
+// var inorderTraversal = function (root) {
+//     let result = [];
+//     function dfs(root) {
+//         if (root === null) return;
+//         dfs(root.left);
+//         result.push(root.val);
+//         dfs(root.right);
+//     }
+//     dfs(root);
+//     return result;
+// };
+
+function inorderTraversal(root) {
+  let result = [];
+  let st = [];
+  let curr = root;
+  while (curr != null || st.length > 0) {
+    while (curr != null) {
+      st.push(curr);
+      curr = curr.left;
     }
-    dfs(root);
-    return result;
-};
+    curr = st.pop();
+    result.push(curr.val);
+    curr = curr.right;
+  }
+  return result;
+}
