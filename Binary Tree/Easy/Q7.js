49f0c9b56a87383037db16f8e2dba1b42049f16d
sahbaz23 <amdsahbaz19@gmail.com>
2026-01-30 16:53:17 +0530

    Binary Tree

---
+++ Binary Tree/Easy/Q7.js
@@ -0,0 +1,9 @@
+// Binary Tree
+
+var isSameTree = function(p, q) {
+    if(p===null||q===null){
+        return (p===q)
+    }
+    return (p.val===q.val)&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    
+};
