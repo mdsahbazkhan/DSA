620802f2d938c569ab6208640734c9cac41ff38c
sahbaz23 <amdsahbaz19@gmail.com>
2026-02-04 15:39:24 +0530

    Symmetric Tree

---
+++ Binary Tree/Easy/Q8.js
@@ -0,0 +1,13 @@
+//Symmetric Tree
+
+var isSymmetric = function (root) {
+  return root === null || isSymmetricHelp(root.left, root.right);
+};
+function isSymmetricHelp(left, right) {
+  if (left === null || right === null) return left === right;
+  if (left.val !== right.val) return false;
+  return (
+    isSymmetricHelp(left.left, right.right) &&
+    isSymmetricHelp(left.right, right.left)
+  );
+}
